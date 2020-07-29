944dd40ef6092cfd4a487301607d8534ce91b590
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-07-29 18:37:44 -0500

    deleted double orbit-report folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { OrbitReportComponent } from './orbit-report/orbit-report.component';
 import { OrbitListComponent } from './orbit-list/orbit-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrbitReportComponent,
     OrbitListComponent
   ],
   imports: [
